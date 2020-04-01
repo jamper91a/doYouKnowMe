0b3b706b86eb7ca53711c8471cd8d4b12591afaf
Jorge Andres Moreno Jaimes <jamper91a@gmail.com>
2020-04-02 10:17:42 +1300

    Showing error messages when something happen

--- api/controllers/friend/add.js
+++ api/controllers/friend/add.js
@@ -14,16 +14,27 @@ module.exports = {
 
 
   exits: {
-
+    nameAlreadyTaken:{
+      description: 'Name already taken',
+      responseType: 'badRequest'
+    }
   },
 
 
   fn: async function (inputs) {
     //Find the quiz with the email provided
     const quiz = await sails.helpers.quiz.getQuizByEmail(this.req.session.quiz.user.uuid);
-    const friend = await Friend.create({quiz: quiz.id, name: inputs.name, score: 0}).fetch();
-    this.req.session.friend = friend;
-    return friend;
+    //Check no other friend has the same name
+    const exitsFriend = await Friend.find({quiz: quiz.id, name: inputs.name});
+    if(!exitsFriend) {
+      const friend = await Friend.create({quiz: quiz.id, name: inputs.name, score: 0}).fetch();
+      this.req.session.friend = friend;
+      return friend;
+    } else {
+      throw {nameAlreadyTaken:[]};
+    }
+
+
 
   }
 
