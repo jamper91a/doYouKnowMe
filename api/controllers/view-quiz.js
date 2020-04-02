56c8b2021cf79e1984ba039b58a9a48c60534342
Jorge Andres Moreno Jaimes <jamper91a@gmail.com>
2020-04-02 20:17:32 +1300

    Solving issue when user try to connect to a test that does not exists

--- api/controllers/view-quiz.js
+++ api/controllers/view-quiz.js
@@ -13,30 +13,39 @@ module.exports = {
 
     success: {
       viewTemplatePath: 'pages/quiz'
+    },
+    redirect: {
+      viewTemplatePath: 'pages/home'
     }
 
   },
 
 
   fn: async function ({email}) {
-    const quiz = await sails.helpers.quiz.getQuizByEmail(email);
-    //Get friends
-    const friends = await Friend.find({quiz: quiz.id}).sort('score DESC');
+    try {
+      const quiz = await sails.helpers.quiz.getQuizByEmail(email);
+      //Get friends
+      const friends = await Friend.find({quiz: quiz.id}).sort('score DESC');
 
-    this.req.session.quiz = quiz;
+      this.req.session.quiz = quiz;
 
-    let meta = {
-      url:  sails.config.custom.baseUrl + 'quiz/' + quiz.user.uuid,
-      title: sails.__('Do you know') + ' ' +quiz.user.name + '?',
-      description: sails.__('Check how well you know') + ' ' + quiz.user.name,
-      image: 'https://previews.123rf.com/images/deltaart/deltaart1411/deltaart141100002/33168823-do-you-know-.jpg',
+      let meta = {
+        url: sails.config.custom.baseUrl + 'quiz/' + quiz.user.uuid,
+        title: sails.__('Do you know') + ' ' + quiz.user.name + '?',
+        description: sails.__('Check how well you know') + ' ' + quiz.user.name,
+        image: 'https://previews.123rf.com/images/deltaart/deltaart1411/deltaart141100002/33168823-do-you-know-.jpg',
+      };
+      // All done.
+      return {
+        meta,
+        quiz,
+        friends
+      };
+    } catch (e) {
+      console.log(e.code);
+      throw { redirect: {error: e.code} };
+      // return exits.redirect({e});
     }
-    // All done.
-    return {
-      meta,
-      quiz,
-      friends
-    };
 
   }
 
